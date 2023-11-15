7b621c358045d9234993512c33e50b002a8e37ae
Cristian Lopez <lopezcristianj@gmail.com>
2023-11-15 18:53:50 +0100

    Removing store stuff

--- src/store/actions/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-// NOTE: global action that can be listened by multiple reducers
-// (this is why it is not included in a particual slice, but as a standalone action itself)
-
-export const reset = createAction("store/reset");
--- src/store/apis/sampleApi.js
+++
@@ -1,5 +0,0 @@
-import { createApi } from "@reactjs/toolkit/query/react";
-
-const sampleApi = createApi({});
-
-// NOTE: sample React Query API config.
